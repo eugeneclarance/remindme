b37c7d2d09df5d425d2e3d7698090b585132a579
Eugene Clarance <eugeneclarance@android-3aaa749acde9bc67.localdomain>
2018-10-12 19:05:46 +0700

    fixing debugging error

--- chatbot/__init__.py
+++ chatbot/__init__.py
@@ -52,6 +52,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event)
+    print(event["source"]["userId"])
     text = event.message.text
     userid = event["source"]["userId"]
 
